9e4c6ac9aa0a91bac93d76aaa062aca16dc7ec1f
angelinaturchyn <angelinaturchyn@gmail.com>
2022-11-12 01:02:45 -0600

    added session info

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-import telebot
-import keys as key
 import logging
 import time
+import telebot
+import keys as key
+import aiogram
+
 
 bot = telebot.TeleBot(key.API_KEY)
 
 logging.basicConfig(level=logging.INFO)
 
 print("Bot started")
+
+
 @bot.message_handler(commands=['hi', 'hello', 'hey'])
-async def help(message):
-    bot.send_message(message.chat.id, "Howdy, how are you doing?")
+def hello(message):
+    bot.send_message(message.chat.id, f"Howdy, {message.from_user.first_name} how are you doing?")
     user_id = message.from_user.id
+    user_first_name = message.from_user.first_name
     user_full_name = message.from_user.full_name
     logging.info(f'{user_id} {user_full_name} {time.asctime()}')
+
 
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id, "How can I help you sweetheart?")
+
+@bot.message_handler()
+def get_user_session_info(message):
+    if message.text == ['user']:
+        bot.send_message(message.chat.id, "Hi user", parse_mode='html')
+    elif message.text == 'id':
+        bot.send_message(message.chat.id, f"Your user_id is: {message.from_user.id}",  parse_mode='html')
+
+    bot.send_message(message.chat.id, message, parse_mode='html')
+
 
 bot.infinity_polling()
